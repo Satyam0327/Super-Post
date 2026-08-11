530627092f14f72e7b434a765665a88f280451b2
satyam singh <101057238+Satyam0327@users.noreply.github.com>
2026-08-11 15:44:17 +0530

    Update Firebase configuration details
    Updated Firebase configuration with new project details and removed database name.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,17 +3,18 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "nifty-backbone-r83b3",
-  appId: "1:436433591620:web:b744df9d5295f467d5cb11",
-  apiKey: "AIzaSyBkYMYLyF9yGH_9rC2JXkmcqBmbiGeCMPg",
-  authDomain: "nifty-backbone-r83b3.firebaseapp.com",
-  storageBucket: "nifty-backbone-r83b3.firebasestorage.app",
-  messagingSenderId: "436433591620"
+  apiKey: "AIzaSyAyhW8ZTTA33NfW52D3TOABGAPjdgfm7Z4",
+  authDomain: "superpost-ec5b8.firebaseapp.com",
+  projectId: "superpost-ec5b8",
+  storageBucket: "superpost-ec5b8.firebasestorage.app",
+  messagingSenderId: "326507440936",
+  appId: "1:326507440936:web:e4efc5c240a2d9abe2fe1d",
+  measurementId: "G-CB0KFRNWD1"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app, "ai-studio-0524ddbb-7588-4600-be6b-e0c560bde6a9");
+const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, db, googleProvider };
